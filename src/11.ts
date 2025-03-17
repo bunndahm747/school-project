f2dd87651002afa4e21f79b568a4519707f91264
bunndahm747 <bunndahm747@gmail.com>
2025-03-17 06:05:28 +0000

    Add 11.ts

---
+++ src/11.ts
@@ -0,0 +1,6 @@
+// This function takes in a string and returns its length
+function countCharacters(inputString: string): number {
+  return inputString.length;
+}
+
+console.log(countCharacters("hello world"));
