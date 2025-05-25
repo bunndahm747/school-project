91b8f10f67ff8179f90b601d78509da683111012
bunndahm747 <bunndahm747@gmail.com>
2025-05-25 06:51:41 +0000

    Add 41.ts

---
+++ src/41.ts
@@ -0,0 +1,9 @@
+let score = 85;
+
+if (score >= 90) {
+    console.log("You did exceptionally well!");
+} else if (score >= 70) {
+    console.log("You scored well. Keep it up.");
+} else {
+    console.log("Keep learning, and you'll reach the top!");
+}
